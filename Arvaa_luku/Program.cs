13fc10acc0799a8b926e19d11b4ae91f2b41145a
A F M Muzahidur Rahman <tareq.rahman06@gmail.com>
2020-12-15 11:02:55 +0200

    added folder

---
+++ Arvaa_luku/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Arvaa_luku
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int randomNumber = new Random().Next(1, 20);
+            Console.WriteLine("Guess a number between 1 and 20");
+
+            static int Guess(){
+                int guess = 0;
+                try {
+                    guess = int.Parse(Console.ReadLine());
+                } catch (Exception){
+                    Console.WriteLine("You did not enter valid guess.");
+                    guess = Guess();
+                }
+                return guess;
+                
+            }
+
+            int tries = 3;
+            int numOfTries = 0 ;
+
+            while (true){
+                int guess = Guess();
+                numOfTries++;
+                if(guess == randomNumber){
+                    Console.WriteLine("Your guess was correct");
+                    break;
+                }
+                if(guess > randomNumber){
+                    Console.WriteLine("Your guess was too high");
+                }
+                if(guess < randomNumber){
+                    Console.WriteLine("Your guess was too low");
+                }
+                if(numOfTries == tries){
+                    Console.WriteLine($"The number is :{randomNumber}");
+                    break;
+                }
+                Console.WriteLine($"You have {tries - numOfTries} tries left. Enter another number: ");
+            }
+
+        }
+
+    }
+}
---
+++ exercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intVal1, intVal2, result;
+            string userInput;
+
+            Console.WriteLine("Enter integer 1: ");
+            userInput = Console.ReadLine();
+            intVal1 = Convert.ToInt32(userInput);
+            Console.WriteLine("Enter integer 2: ");
+            userInput = Console.ReadLine();
+            intVal2 = Convert.ToInt32(userInput);
+            result = intVal1 + intVal2;
+            Console.WriteLine("{0} + {1} = {2}", intVal1, intVal2, result);
+
+        }
+    }
+}
